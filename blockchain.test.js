c6a18838bd963dd32676d59455586c8c0fa4ce0f
Ádám Csaba <adam.csaba.84@gmail.com>
2021-09-17 07:56:28 +0200

    Add .editorconfig, average work POC with hex leading zeros difficulty

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,132 +2,132 @@ const Blockchain = require("./blockchain");
 const Block = require("./block");
 
 describe("Blockchain", () => {
-  let blockchain;
-  let newChain;
-  let originalChain;
-
-  beforeEach(() => {
-    blockchain = new Blockchain();
-    newChain = new Blockchain();
-
-    originalChain = blockchain.chain;
-  });
-
-  it("contains a `chain` Array instance", () => {
-    expect(blockchain.chain instanceof Array).toBe(true);
-  });
-
-  it("starts with the genesis block", () => {
-    expect(blockchain.chain[0]).toEqual(Block.genesis());
-  });
-
-  it("adds a new block to the chain", () => {
-    const newData = "foo bar";
-    blockchain.addBlock({ data: newData });
-
-    expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
-  });
-
-  describe("isValidChain()", () => {
-    describe("when the chain does not start with the genesis block", () => {
-      it("returns false", () => {
-        blockchain.chain[0] = { data: "fake-genesis" };
-
-        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
-      });
-    });
-
-    describe("when the chain starts with the genesis block and has multiple blocks", () => {
-      beforeEach(() => {
-        blockchain.addBlock({ data: "Bears" });
-        blockchain.addBlock({ data: "Beats" });
-        blockchain.addBlock({ data: "Battlestar Galactica" });
-      });
-
-      describe("and lastHash reference has changed", () => {
-        it("returns false", () => {
-          blockchain.chain[2].lastHash = "broken-lastHash";
-          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
-        });
-      });
-
-      describe("and the chain contains a block with an invalid field", () => {
-        it("returns false", () => {
-          blockchain.chain[2].data = "bad-data";
-          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
-        });
-      });
-
-      describe("and the chain does not contain any ivalid blocks", () => {
-        it("returns true", () => {
-          expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
-        });
-      });
-    });
-  });
-
-  describe("replaceChain()", () => {
-    let errorMock, logMock;
+    let blockchain;
+    let newChain;
+    let originalChain;
 
     beforeEach(() => {
-      errorMock = jest.fn();
-      logMock = jest.fn();
+        blockchain = new Blockchain();
+        newChain = new Blockchain();
 
-      global.console.error = errorMock;
-      global.console.log = logMock;
+        originalChain = blockchain.chain;
     });
 
-    describe("when the new chain is not longer", () => {
-      beforeEach(() => {
-        newChain.chain[0] = { new: "chain" };
-
-        blockchain.replaceChain(newChain.chain);
-      });
-
-      it("does not replace the chain", () => {
-        expect(blockchain.chain).toEqual(originalChain);
-      });
-
-      it("logs an error", () => {
-        expect(errorMock).toHaveBeenCalled();
-      });
+    it("contains a `chain` Array instance", () => {
+        expect(blockchain.chain instanceof Array).toBe(true);
     });
 
-    describe("when the new chain is longer", () => {
-      beforeEach(() => {
-        newChain.addBlock({ data: "Bears" });
-        newChain.addBlock({ data: "Beats" });
-        newChain.addBlock({ data: "Battlestar Galactica" });
-      });
+    it("starts with the genesis block", () => {
+        expect(blockchain.chain[0]).toEqual(Block.genesis());
+    });
 
-      describe("and the chain is invald", () => {
+    it("adds a new block to the chain", () => {
+        const newData = "foo bar";
+        blockchain.addBlock({ data: newData });
+
+        expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+    });
+
+    describe("isValidChain()", () => {
+        describe("when the chain does not start with the genesis block", () => {
+            it("returns false", () => {
+                blockchain.chain[0] = { data: "fake-genesis" };
+
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
+
+        describe("when the chain starts with the genesis block and has multiple blocks", () => {
+            beforeEach(() => {
+                blockchain.addBlock({ data: "Bears" });
+                blockchain.addBlock({ data: "Beats" });
+                blockchain.addBlock({ data: "Battlestar Galactica" });
+            });
+
+            describe("and lastHash reference has changed", () => {
+                it("returns false", () => {
+                    blockchain.chain[2].lastHash = "broken-lastHash";
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
+
+            describe("and the chain contains a block with an invalid field", () => {
+                it("returns false", () => {
+                    blockchain.chain[2].data = "bad-data";
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
+
+            describe("and the chain does not contain any ivalid blocks", () => {
+                it("returns true", () => {
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+                });
+            });
+        });
+    });
+
+    describe("replaceChain()", () => {
+        let errorMock, logMock;
+
         beforeEach(() => {
-          newChain.chain[2].hash = "some-fake-hash";
+            errorMock = jest.fn();
+            logMock = jest.fn();
 
-          blockchain.replaceChain(newChain.chain);
-        });
-        it("does not replace the chain", () => {
-          expect(blockchain.chain).toEqual(originalChain);
-        });
-
-        it("logs an error", () => {
-          expect(errorMock).toHaveBeenCalled();
-        });
-      });
-
-      describe("and the chain is valid", () => {
-        beforeEach(() => {
-          blockchain.replaceChain(newChain.chain);
+            global.console.error = errorMock;
+            global.console.log = logMock;
         });
 
-        it("replaces the chain", () => {
-          expect(blockchain.chain).toEqual(newChain.chain);
+        describe("when the new chain is not longer", () => {
+            beforeEach(() => {
+                newChain.chain[0] = { new: "chain" };
+
+                blockchain.replaceChain(newChain.chain);
+            });
+
+            it("does not replace the chain", () => {
+                expect(blockchain.chain).toEqual(originalChain);
+            });
+
+            it("logs an error", () => {
+                expect(errorMock).toHaveBeenCalled();
+            });
         });
 
-        it("logs about the replacement", () => {
-          expect(logMock).toHaveBeenCalled();
+        describe("when the new chain is longer", () => {
+            beforeEach(() => {
+                newChain.addBlock({ data: "Bears" });
+                newChain.addBlock({ data: "Beats" });
+                newChain.addBlock({ data: "Battlestar Galactica" });
+            });
+
+            describe("and the chain is invald", () => {
+                beforeEach(() => {
+                    newChain.chain[2].hash = "some-fake-hash";
+
+                    blockchain.replaceChain(newChain.chain);
+                });
+                it("does not replace the chain", () => {
+                    expect(blockchain.chain).toEqual(originalChain);
+                });
+
+                it("logs an error", () => {
+                    expect(errorMock).toHaveBeenCalled();
+                });
+            });
+
+            describe("and the chain is valid", () => {
+                beforeEach(() => {
+                    blockchain.replaceChain(newChain.chain);
+                });
+
+                it("replaces the chain", () => {
+                    expect(blockchain.chain).toEqual(newChain.chain);
+                });
+
+                it("logs about the replacement", () => {
+                    expect(logMock).toHaveBeenCalled();
+                });
+            });
         });
-      });
     });
-  });
 });
